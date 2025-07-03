2578b8a5c6a9134ad94c5f25cba85175b04bae8b
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-03 21:31:30 +0700

    put ir re

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from typing import Union
 
 from fastapi import FastAPI
-import speech_recognition as sr
-r = sr.Recognizer()
+import config_pinout
+import RPi.GPIO as GPIO
+# import speech_recognition as sr
+# r = sr.Recognizer()
 app = FastAPI()
 
 
@@ -16,22 +18,34 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-with sr.Microphone() as source:
-    print("Bắt đầu nhận giọng nói... (Ctrl+C để dừng)")
 
-    while True:
-        try:
-            print("⏺️ Đang lắng nghe...")
-            audio = r.listen(source)
 
-            print("🔍 Đang xử lý...")
-            text = r.recognize_google(audio, language="vi-VN")
-            print("🗣️ Bạn nói:", text)
+def setup_ir():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(config_pinout.GPIO_IR_RECEIVER, GPIO.IN)
 
-        except sr.UnknownValueError:
-            print("⚠️ Không hiểu được âm thanh.")
-        except sr.RequestError as e:
-            print("❌ Lỗi kết nối API:", e)
-        except KeyboardInterrupt:
-            print("\n🛑 Dừng lại bởi người dùng.")
-            break
+def read_ir():
+    if GPIO.input(config_pinout.GPIO_IR_RECEIVER) == GPIO.LOW:
+        print("IR nhận tín hiệu")
+while True:
+    read_ir()
+
+# with sr.Microphone() as source:
+#     print("Bắt đầu nhận giọng nói... (Ctrl+C để dừng)")
+#
+#     while True:
+#         try:
+#             print("⏺️ Đang lắng nghe...")
+#             audio = r.listen(source)
+#
+#             print("🔍 Đang xử lý...")
+#             text = r.recognize_google(audio, language="vi-VN")
+#             print("🗣️ Bạn nói:", text)
+#
+#         except sr.UnknownValueError:
+#             print("⚠️ Không hiểu được âm thanh.")
+#         except sr.RequestError as e:
+#             print("❌ Lỗi kết nối API:", e)
+#         except KeyboardInterrupt:
+#             print("\n🛑 Dừng lại bởi người dùng.")
+#             break
