a51f7d0c2fc39c19ea250be3f0860424f33b9740
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-03 22:30:57 +0700

    put ir re

--- ir.py
+++ ir.py
@@ -12,7 +12,7 @@ pi.set_mode(config_pinout.GPIO_IR_RECEIVER, pigpio.INPUT)
 cb = pi.callback(config_pinout.GPIO_IR_RECEIVER, pigpio.FALLING_EDGE, cbf)
 
 print("Đang đọc xung IR... Nhấn nút trên remote.")
-time.sleep(5)
+time.sleep(15)
 
 cb.cancel()
 pi.stop()
