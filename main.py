4be6cb8e9646e1236f0ce4a304c90d041659144c
tinhvd <dangtinha2@gmail.com>
2025-07-03 16:29:06 +0700

    update speech to text demo

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
-
+import speech_recognition as sr
+r = sr.Recognizer()
 app = FastAPI()
 
 
@@ -13,3 +14,24 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+with sr.Microphone() as source:
+    print("Bắt đầu nhận giọng nói... (Ctrl+C để dừng)")
+
+    while True:
+        try:
+            print("⏺️ Đang lắng nghe...")
+            audio = r.listen(source)
+
+            print("🔍 Đang xử lý...")
+            text = r.recognize_google(audio, language="vi-VN")
+            print("🗣️ Bạn nói:", text)
+
+        except sr.UnknownValueError:
+            print("⚠️ Không hiểu được âm thanh.")
+        except sr.RequestError as e:
+            print("❌ Lỗi kết nối API:", e)
+        except KeyboardInterrupt:
+            print("\n🛑 Dừng lại bởi người dùng.")
+            break
