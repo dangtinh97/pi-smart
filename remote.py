1cd71b33d1461651565e8da2c3502b2f7dd6b25d
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-07 19:39:34 +0700

    play audio new

--- remote.py
+++ remote.py
@@ -1,18 +1,13 @@
 import pigpio
 import time
 
-GPIO_IR = 13  # ✅ Sửa từ 18 → 13
-
 pi = pigpio.pi()
-if not pi.connected:
-    exit(1)
+pi.set_mode(13, pigpio.OUTPUT)
+pi.set_PWM_frequency(13, 38000)
+pi.set_PWM_dutycycle(13, 128)
 
-pi.set_mode(GPIO_IR, pigpio.OUTPUT)
-pi.set_PWM_frequency(GPIO_IR, 38000)      # 38kHz carrier
-pi.set_PWM_dutycycle(GPIO_IR, 128)        # Duty cycle ~50%
-
-print("Đang gửi sóng IR...")
+print("PWM đang phát 38kHz trên GPIO13 (5 giây)...")
 time.sleep(5)
 
-pi.set_PWM_dutycycle(GPIO_IR, 0)
+pi.set_PWM_dutycycle(13, 0)
 pi.stop()
