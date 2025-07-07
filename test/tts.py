2a9face48661e913506b2e11f47a6cc1e236d63b
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-07 13:55:33 +0700

    put ir re

---
+++ test/tts.py
@@ -0,0 +1,19 @@
+from gtts import gTTS
+import os
+from config import PATH_MPG123
+def play_voice(mytext):
+
+    # Language for the speech
+    language = 'vi'
+
+    # Create a gTTS object
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+
+    # Save the converted audio to an MP3 file
+    myobj.save("./welcome.mp3")
+
+    # Optionally, play the audio file (requires an audio player)
+    # os.system("start welcome.mp3") # For Windows
+    # os.system("mpg321 welcome.mp3") # For Linux
+    os.system(PATH_MPG123+" ./welcome.mp3")
+    os.remove("./welcome.mp3")
