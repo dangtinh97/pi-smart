818c44c67859b22d536d4bb8758e0c732d165122
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-07 15:09:24 +0700

    put ir re

--- handlers/wakeword_handler.py
+++ handlers/wakeword_handler.py
@@ -8,9 +8,7 @@ is_listening = False
 from services.text_to_speech import  play_voice
 def on_wakeword_detected():
     global is_listening
-    os.system(PATH_MPG123+" -o alsa -a hw:1,0 ./data/sound.mp3")
-    time.sleep(1)
-    os.system("sudo alsactl init")
+    #os.system(PATH_MPG123+" -o alsa -a hw:1,0 ./data/sound.mp3")
     if is_listening:
         print("⚠️ Đã trong quá trình nhận lệnh, bỏ qua wakeword")
         return
