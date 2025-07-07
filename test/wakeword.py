f5a35968a3c6f3fcb12000f6f4d6462d02c94f10
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-07 14:15:38 +0700

    put ir re

--- test/wakeword.py
+++ test/wakeword.py
@@ -1,15 +1,13 @@
-import pvporcupine
 import pyaudio
-porcupine = pvporcupine.create(
-            access_key="I2FzB0ROEKTLiBLnLa8jByF9b7wu+o6h4Z9PvWFKRwTpmZ9gmBpeaw==",
-            keyword_paths=["data/hotwords/raspberry.ppn"]
-        )
+
 pa = pyaudio.PyAudio()
-# stream = pa.open(
-#     rate=porcupine.sample_rate,
-#     channels=1,
-#     format=pyaudio.paInt16,
-#     input=True,
-#     frames_per_buffer=porcupine.frame_length,
-#     input_device_index=1
-# )
+
+stream = pa.open(
+    rate=16000,
+    channels=1,
+    format=pyaudio.paInt16,
+    input=True,
+    frames_per_buffer=1024,
+)
+
+print("Ghi âm OK!")
