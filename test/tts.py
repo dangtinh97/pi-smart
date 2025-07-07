4c1a090eef56e8392febb4b3b971c520e61dbd4e
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-07 13:56:33 +0700

    put ir re

--- test/tts.py
+++ test/tts.py
@@ -1,6 +1,5 @@
 from gtts import gTTS
 import os
-from config import PATH_MPG123
 def play_voice(mytext):
 
     # Language for the speech
@@ -15,7 +14,7 @@ def play_voice(mytext):
     # Optionally, play the audio file (requires an audio player)
     # os.system("start welcome.mp3") # For Windows
     # os.system("mpg321 welcome.mp3") # For Linux
-    os.system(PATH_MPG123+" ./welcome.mp3")
+    os.system("/usr/bin/mpg123 ./welcome.mp3")
     os.remove("./welcome.mp3")
 
 play_voice("Xin xin")
