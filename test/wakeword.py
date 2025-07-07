257ae98f81bb0cfdb083c7bd6336aa1b95f075eb
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-07 14:01:23 +0700

    put ir re

--- test/wakeword.py
+++ test/wakeword.py
@@ -1,4 +1,5 @@
 import pvporcupine
+import pyaudio
 porcupine = pvporcupine.create(
             access_key="I2FzB0ROEKTLiBLnLa8jByF9b7wu+o6h4Z9PvWFKRwTpmZ9gmBpeaw==",
             keyword_paths=["data/hotwords/raspberry.ppn"]
