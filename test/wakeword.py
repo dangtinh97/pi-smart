f85d2d6f0a494a988dfd2f898cb86ff35b762f83
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-07 14:00:31 +0700

    put ir re

--- test/wakeword.py
+++ test/wakeword.py
@@ -1,7 +1,7 @@
 import pvporcupine
 porcupine = pvporcupine.create(
-            access_key=ACCESS_KEY,
-            keyword_paths=[KEYWORD_PATH]
+            access_key="I2FzB0ROEKTLiBLnLa8jByF9b7wu+o6h4Z9PvWFKRwTpmZ9gmBpeaw==",
+            keyword_paths=["data/hotwords/raspberry.ppn"]
         )
 pa = pyaudio.PyAudio()
 stream = pa.open(
