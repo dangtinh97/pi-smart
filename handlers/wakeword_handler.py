47329b47be19e166226d9babef761a0d880b5231
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-07 16:00:50 +0700

    play audio new

--- handlers/wakeword_handler.py
+++ handlers/wakeword_handler.py
@@ -4,6 +4,7 @@ from services import speech_recognizer
 from wakeword.porcupine_listener import wakeword_listener
 import os
 from config import PATH_MPG123
+from services.play_audio import play_audio_wav
 is_listening = False
 lock = threading.Lock()
 from services.text_to_speech import  play_voice
@@ -11,7 +12,8 @@ def on_wakeword_detected():
     global is_listening
     def play_sound():
         try:
-            os.system(f"{PATH_MPG123} -o alsa -a hw:1,0 ./data/sound.mp3")
+            play_audio_wav("./data/sound.wav")
+            #os.system(f"{PATH_MPG123} -o alsa -a hw:1,0 ./data/sound.mp3")
         except Exception as e:
             print(f"Error play sound.mp3: {e}")
 
--- services/play_audio.py
+++ services/play_audio.py
@@ -1,13 +1,6 @@
-import pygame
+import simpleaudio as sa
 
-def play_audio_mp3(path="data/sound.mp3"):
-    try:
-        pygame.mixer.init()
-        pygame.mixer.music.load(path)
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy():
-            time.sleep(0.1)
-    except Exception as e:
-        logger.error(f"🔊 Lỗi khi phát bằng pygame: {e}")
-    finally:
-        pygame.mixer.quit()
+def play_audio_wav(filepath):
+    wave_obj = sa.WaveObject.from_wave_file(filepath)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
