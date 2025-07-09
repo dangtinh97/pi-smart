6198fe0f9bba2481ce5f44cca31e0253b8b86651
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-10 02:13:03 +0700

    play audio new

--- test/show_led.py
+++ test/show_led.py
@@ -10,3 +10,6 @@ serial = i2c(port=1, address=0x3C)
 
 # substitute ssd1331(...) or sh1106(...) below if using that device
 device = ssd1306(serial)
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="white", fill="black")
+    draw.text((30, 40), "Hello World", fill="white")
