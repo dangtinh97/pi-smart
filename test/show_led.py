78f844c4dfcf7b516dd52f040ac18e1bbb871f97
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-10 01:44:53 +0700

    play audio new

--- test/show_led.py
+++ test/show_led.py
@@ -1,28 +1,30 @@
-from PIL import Image, ImageDraw, ImageFont
-import board, busio
+import board
+import busio
 from adafruit_ssd1306 import SSD1306_I2C
+from PIL import Image, ImageDraw, ImageFont
 
-# Tạo kết nối I2C và màn hình
+# Tạo I2C interface
 i2c = busio.I2C(board.SCL, board.SDA)
+
+# Khởi tạo OLED 128x64
 oled = SSD1306_I2C(128, 64, i2c)
 
 # Xoá màn hình
 oled.fill(0)
 oled.show()
 
-# Tạo khung vẽ ảnh trắng đen
+# Tạo ảnh đen trắng
 image = Image.new("1", (oled.width, oled.height))
 draw = ImageDraw.Draw(image)
 
-# Load font mặc định hoặc font TrueType
+# Font mặc định
 font = ImageFont.load_default()
-# font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 14)
 
 # Ghi nội dung
-draw.text((0, 0), "Xin chao Dai ca!", font=font, fill=255)
-draw.text((0, 20), "Man OLED 128x64", font=font, fill=255)
-draw.text((0, 40), "Sang & Rach net", font=font, fill=255)
+draw.text((0, 0), "OLED 128x64", font=font, fill=255)
+draw.text((0, 20), "Dia chi I2C: 0x3C", font=font, fill=255)
+draw.text((0, 40), "Chao Dai ca!", font=font, fill=255)
 
-# Hiển thị lên màn hình
+# Gửi ảnh lên màn
 oled.image(image)
 oled.show()
