efd9e4bed08915493539399c71d2c746be13fbc2
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-10 02:13:44 +0700

    play audio new

--- test/show_led.py
+++ test/show_led.py
@@ -1,15 +1,13 @@
-from luma.core.interface.serial import i2c, spi, pcf8574
-from luma.core.interface.parallel import bitbang_6800
+from luma.core.interface.serial import i2c
 from luma.core.render import canvas
-from luma.oled.device import ssd1306, ssd1309, ssd1325, ssd1331, sh1106, sh1107, ws0010
+from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
+from time import sleep
 
-# rev.1 users set port=0
-# substitute spi(device=0, port=0) below if using that interface
-# substitute bitbang_6800(RS=7, E=8, PINS=[25,24,23,27]) below if using that interface
 serial = i2c(port=1, address=0x3C)
+device = ssd1306(serial, rotate=1)
 
-# substitute ssd1331(...) or sh1106(...) below if using that device
-device = ssd1306(serial)
+# Box and text rendered in portrait mode
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, outline="white", fill="black")
-    draw.text((30, 40), "Hello World", fill="white")
+    draw.text((10, 40), "Hello World", fill="white")
+sleep(10)
