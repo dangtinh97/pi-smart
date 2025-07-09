3d20544046bf1cf568fa74c6aa7a99fad4b95685
dangtinhvu <dangtinhhaui@gmail.com>
2025-07-10 02:22:15 +0700

    play audio new

--- test/show_led.py
+++ test/show_led.py
@@ -4,10 +4,10 @@ from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
 from time import sleep
 
 serial = i2c(port=1, address=0x3C)
-device = sh1106(serial, rotate=0)
+device = sh1106(serial, rotate=2)
 
 # Box and text rendered in portrait mode
 with canvas(device) as draw:
     draw.rectangle(device.bounding_box, outline="white", fill="black")
     draw.text((10, 40), "Hello World", fill="white")
-sleep(10)
+sleep(100)
